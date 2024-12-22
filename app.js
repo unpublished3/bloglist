1fe8cc6912c3e904a1182107ef72b98630228de8
unpublished3 <chapagainsakshyam@gmail.com>
2024-12-22 08:51:16 +0545

    Add middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 
 const config = require("./utils/config")
 const notesRouter = require("./controllers/notes")
+const middleware = require("./utils/middleware")
 
 mongoose.connect(config.MONGODB_URI)
 
@@ -11,7 +12,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", notesRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
